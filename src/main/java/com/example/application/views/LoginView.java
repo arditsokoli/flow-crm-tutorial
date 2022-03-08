3f867a0e4c2da855e21597fc9d8f655c9eaf9615
Ardit Sokoli <36007494+arditsokoli@users.noreply.github.com>
2022-03-08 04:15:12 +0100

    edit

--- src/main/java/com/example/application/views/LoginView.java
+++ src/main/java/com/example/application/views/LoginView.java
@@ -1,6 +1,7 @@
 package com.example.application.views;
 
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
@@ -23,6 +24,7 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 		login.setAction("login");
 
 		add(new H1("Vaadin CRM"), login);
+		add(new H4("Add for user: user &  password: userpass"), login);
 	}
 
 	@Override
